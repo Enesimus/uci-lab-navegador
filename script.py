e82b7781d2a141a0f430c10effc1cff419e1843a
Juan Sepulveda <jrsepulv@gmail.com>
2026-02-09 23:22:32 -0300

    archivos iniciales

---
+++ script.py
@@ -0,0 +1,33 @@
+'''
+Docstring para script
+'''
+
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+
+html = open("prueba.html", encoding=None).read()
+soup = bs(html, "html.parser")
+
+resultados = []
+
+for fila in soup.select("tr.grid-row"):
+    dato = {}
+    for celda in fila.select("td.grid-cell"):
+        campo = celda.get("data-name")
+        valor = celda.get_text(strip= True)
+        dato[campo] = valor
+    resultados.append(dato)
+
+orden = {}
+
+for fila in soup.select("h4.modal-title"):
+    dato2 = []
+    valor = fila.get_text(strip=True)
+    dato2 = valor.split()
+    orden_num = dato2[3]
+    orden["Numero Orden"] = orden_num
+
+# dato2.append(valor)
+
+df = pd.DataFrame(resultados,)
+
